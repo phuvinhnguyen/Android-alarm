45cf12fa1c83ebc183f90a5a77f82c640b2ad13e
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2022-08-31 00:21:31 +0700

    'new'

--- app/src/main/java/com/example/alarmmanagement/easyReceiver.java
+++ app/src/main/java/com/example/alarmmanagement/easyReceiver.java
@@ -1,9 +1,12 @@
 package com.example.alarmmanagement;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -22,6 +25,20 @@ public class easyReceiver extends BroadcastReceiver {
 
         //start sound service
         context.startService(intent1);
+
+        int NOTIFICATION_ID = 234;
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "Wake up";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
 
         //make notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID)
